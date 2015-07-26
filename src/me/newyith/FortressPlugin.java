146383f8b5c89b62b688b1d8b38260f99abdcb17
Rocky Sims <rocky.sims@gmail.com>
2015-07-26 04:29:18 -0700

    Removed /hello command.

--- src/me/newyith/FortressPlugin.java
+++ src/me/newyith/FortressPlugin.java
@@ -1,8 +1,5 @@
 package me.newyith;
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import java.util.logging.Logger;
 
@@ -18,14 +15,5 @@ public class FortressPlugin extends JavaPlugin {
     @Override
     public void onDisable() {
         log.info("FortressPlugin onDisable called");
-    }
-
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if (cmd.getName().equalsIgnoreCase("hello") && sender instanceof Player) {
-            Player player = (Player) sender;
-            player.sendMessage("Hello, " + player.getName() + ".");
-            return true;
-        }
-        return false;
     }
 }
