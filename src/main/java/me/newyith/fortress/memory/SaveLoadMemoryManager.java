575e61bdce780c99017bb4d060796442fc6d9734
Rocky Sims <rocky.sims@gmail.com>
2015-10-11 01:28:45 -0700

    Cleaned up some commented out code

--- src/main/java/me/newyith/fortress/memory/SaveLoadMemoryManager.java
+++ src/main/java/me/newyith/fortress/memory/SaveLoadMemoryManager.java
@@ -30,7 +30,7 @@ public class SaveLoadMemoryManager {
 			MapMemory memory = new MapMemory(memMap);
 			MapMemory m = new MapMemory(memory.section("RunesManager"));
 
-			Debug.start("loadFrom"); //slow and scales roughly linearly
+			Debug.start("loadFrom"); //slow and scales poorly
 			FortressGeneratorRunesManager.loadFrom(m);
 			Debug.end("loadFrom");
 		} catch (Exception e) {
@@ -68,91 +68,4 @@ public class SaveLoadMemoryManager {
 		}
 		Debug.end("save");
 	}
-
-
-
-
-	//TODO: reuse this for actual config file in FortressPlugin
-//	public static void onEnable_old(FortressPlugin plugin) {
-//		Memory memory = new Memory(plugin.getConfig());
-//		Memory m = new Memory(memory.section("RunesManager"));
-//
-//		FortressGeneratorRunesManager.loadFrom(m);
-//	}
-//
-//	public static void onDisable_old(FortressPlugin plugin) {
-//		//clear config
-//		for(String key : plugin.getConfig().getKeys(false)){
-//			plugin.getConfig().set(key, null);
-//		}
-//
-//		Memory memory = new Memory(plugin.getConfig());
-//		Memory m = new Memory(memory.section("RunesManager"));
-//
-//		FortressGeneratorRunesManager.saveTo(m);
-//
-//		plugin.saveConfig();
-//	}
 }
-
-
-
-//		File folder = Bukkit.getServer().getPluginManager().getPlugin("FortressPlugin").getDataFolder();
-//		Debug.msg("data folder path: " + folder.getAbsolutePath());
-//		File file = new File(folder, "manual.txt");
-//		if (file.exists()) {
-//
-//		}
-
-//		configFile = new File(, "config.yml");
-//		if (configFile.exists()) {
-//			myConfig = new YamlConfiguration();
-//			try {
-//				myConfig.load(configFile);
-//			} catch (FileNotFoundException ex) {
-//				// TODO: Log exception
-//			} catch (IOException ex) {
-//				// TODO: Log exception
-//			} catch (InvalidConfigurationException ex) {
-//				// TODO: Log exception
-//			}
-//			loaded = true;
-//		}
-
-
-
-
-
-//		PrintWriter writer = null;
-//		try {
-//			writer = new PrintWriter("test.txt", "UTF-8");
-//			writer.println("tested");
-//			writer.close();
-//		} catch (FileNotFoundException e) {
-//			e.printStackTrace();
-//		} catch (UnsupportedEncodingException e) {
-//			e.printStackTrace();
-//		}
-//
-//
-//		try {
-//			BufferedReader br = new BufferedReader(new FileReader("test.txt"));
-//			try {
-//				StringBuilder sb = new StringBuilder();
-//				String line = br.readLine();
-//
-//				while (line != null) {
-//					sb.append(line);
-//					sb.append(System.lineSeparator());
-//					line = br.readLine();
-//				}
-//				String everything = sb.toString();
-//				Debug.msg("everything: " + everything);
-//			} finally {
-//				br.close();
-//			}
-//		} catch (java.io.FileNotFoundException e) {
-//			Debug.error("caught FileNotFoundException: " + e.getMessage());
-//		} catch (java.io.IOException e) {
-//			e.printStackTrace();
-//		}
