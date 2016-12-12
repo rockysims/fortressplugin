85b0665a40c9514ffd5d5f011784d1bbb0924457
Rocky Sims <rocky.sims@gmail.com>
2016-12-11 20:54:57 -0800

    Fixed bug where protection and bedrock authTokens couldn't be saved/loaded by jackson.

--- src/main/java/me/newyith/fortress/bedrock/BedrockAuthToken.java
+++ src/main/java/me/newyith/fortress/bedrock/BedrockAuthToken.java
@@ -2,4 +2,5 @@ package me.newyith.fortress.bedrock;
 
 public class BedrockAuthToken {
 	//just an object
+	private final boolean ph = true; //this placeholder is to make jackson happy
 }
--- src/main/java/me/newyith/fortress/protection/ProtectionAuthToken.java
+++ src/main/java/me/newyith/fortress/protection/ProtectionAuthToken.java
@@ -2,4 +2,5 @@ package me.newyith.fortress.protection;
 
 public class ProtectionAuthToken {
 	//just an object
+	private final boolean ph = true; //this placeholder is to make jackson happy
 }
