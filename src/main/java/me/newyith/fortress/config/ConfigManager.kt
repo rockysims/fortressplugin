25b9037d4951b67bc3164807549aa38f6a897e43
Rocky Sims <rocky.sims@gmail.com>
2017-05-30 21:40:20 -0700

    Made ConfigManager more succinct.

--- src/main/java/me/newyith/fortress/config/ConfigManager.kt
+++ src/main/java/me/newyith/fortress/config/ConfigManager.kt
@@ -6,29 +6,23 @@ import org.bukkit.plugin.java.JavaPlugin
 
 object ConfigManager {
 	fun load(plugin: JavaPlugin): ConfigData {
-		var glowstoneDustBurnTimeMs = 1000 * 60 * 60
-		var stuckDelayMs = 30 * 1000
-		var stuckCancelDistance = 4
-		var generationRangeLimit = 128
-		var generationBlockLimit = 40000 //roughly 125 empty 8x8x8 rooms (6x6x6 air inside)
-
 		val config = plugin.getConfig()
-		if (FortressPlugin.releaseBuild) {
-			glowstoneDustBurnTimeMs = getOrSetInt(config, "glowstoneDustBurnTimeMs", glowstoneDustBurnTimeMs)
-			stuckDelayMs = getOrSetInt(config, "stuckDelayMs", stuckDelayMs)
-			stuckCancelDistance = getOrSetInt(config, "stuckCancelDistance", stuckCancelDistance)
-			generationRangeLimit = getOrSetInt(config, "generationRangeLimit", generationRangeLimit)
-			generationBlockLimit = getOrSetInt(config, "generationBlockLimit", generationBlockLimit)
+
+		if (!FortressPlugin.releaseBuild) {
+			//force use of default config values as defined below (overwrite config file)
+			config.getKeys(false).forEach { config.set(it, 0F) }
 		}
+
+		val configData = ConfigData(
+			getOrSetInt(config, "glowstoneDustBurnTimeMs", 1000 * 60 * 60),
+			getOrSetInt(config, "stuckDelayMs", 30 * 1000),
+			getOrSetInt(config, "stuckCancelDistance", 4),
+			getOrSetInt(config, "generationRangeLimit", 128),
+			getOrSetInt(config, "generationBlockLimit", 40000) //roughly 125 empty 8x8x8 rooms (6x6x6 air inside)
+		)
 		plugin.saveConfig()
 
-		return ConfigData(
-				glowstoneDustBurnTimeMs,
-				stuckDelayMs,
-				stuckCancelDistance,
-				generationRangeLimit,
-				generationBlockLimit
-				)
+		return configData
 	}
 
 	private fun getOrSetInt(config: FileConfiguration, key: String, defaultValue: Int): Int {
--- src/main/java/me/newyith/fortress/main/FortressPluginForWorld.kt
+++ src/main/java/me/newyith/fortress/main/FortressPluginForWorld.kt
@@ -3,17 +3,20 @@ package me.newyith.fortress.main
 import me.newyith.util.Log
 import me.newyith.util.Point
 import org.bukkit.Bukkit
+import org.bukkit.World
 import org.bukkit.event.block.BlockBreakEvent
 
-class FortressPluginForWorld(val worldName :String) {
-	val world = Bukkit.getWorld(worldName)
+class FortressPluginForWorld(val worldName: String) {
+	val world: World? = Bukkit.getWorld(worldName)
 
 	fun enable() {
 		Log.log(worldName + " enable() called")
+		//TODO: load
 	}
 
 	fun disable() {
 		Log.log(worldName + " disable() called")
+		//TODO: save
 	}
 
 	fun  onBlockBreakEvent(event: BlockBreakEvent) {
