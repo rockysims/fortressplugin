e79d80312bda9aab5b5be0689dfa05be3d812abf
Rocky Sims <rocky.sims@gmail.com>
2017-07-29 18:41:43 -0700

    Added Point.isClaimed(), Point.isAiry(), and Location.enforceMinEdgeDist() extension methods.

--- src/main/java/me/newyith/fortress/extension/point/PointExtensions.kt
+++ src/main/java/me/newyith/fortress/extension/point/PointExtensions.kt
@@ -1,9 +1,31 @@
 package me.newyith.fortress.extension.point
 
+import me.newyith.fortress.main.FortressPlugin
 import me.newyith.fortress.protection.ProtectionManager
 import me.newyith.util.Point
+import org.bukkit.Material
 import org.bukkit.World
 
 fun Point.isProtected(world: World): Boolean {
-	return ProtectionManager.forWorld(world).isProtected(this);
+	return ProtectionManager.forWorld(world).isProtected(this)
+}
+
+fun Point.isClaimed(world: World): Boolean {
+	return FortressPlugin.forWorld(world).isClaimed(this)
+}
+
+fun Point.isAiry(world: World): Boolean {
+	return when(this.getType(world)) {
+		Material.AIR,
+		Material.LONG_GRASS,
+		Material.RED_ROSE,
+		Material.YELLOW_FLOWER,
+		Material.DOUBLE_PLANT,
+		Material.DEAD_BUSH,
+		Material.SUGAR_CANE_BLOCK,
+		Material.SAPLING,
+		Material.CROPS,
+		Material.POTATO -> true
+		else -> false
+	}
 }
