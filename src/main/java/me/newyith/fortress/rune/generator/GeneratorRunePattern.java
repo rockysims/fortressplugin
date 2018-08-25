7f539477767ad67ce225f0173f560b46a5db0311
Rocky Sims <rocky.sims@gmail.com>
2018-08-24 17:04:31 -0700

    Updated GeneratorRunePattern to allow trapped chest to be used as generator's fuel chest.

--- src/main/java/me/newyith/fortress/rune/generator/GeneratorRunePattern.java
+++ src/main/java/me/newyith/fortress/rune/generator/GeneratorRunePattern.java
@@ -136,7 +136,7 @@ public class GeneratorRunePattern {
 				Point p = a.add(0, -1, 0).add(towardLeft);
 				Point r = a.add(0, -1, 0);
 				Point f = a.add(0, -1, 0).add(towardRight);
-				if (c.is(Material.REDSTONE_WIRE, world) && w.is(Material.CHEST, world)) {
+				if (c.is(Material.REDSTONE_WIRE, world) && (w.is(Material.CHEST, world) || w.is(Material.TRAPPED_CHEST, world))) {
 					Point t;
 					//reverse wire / chest
 					t = w;
@@ -151,7 +151,7 @@ public class GeneratorRunePattern {
 				//check other blocks match pattern
 				boolean valid = true;
 				valid = valid && w.is(Material.REDSTONE_WIRE, world);
-				valid = valid && c.is(Material.CHEST, world);
+				valid = valid && (c.is(Material.CHEST, world) || c.is(Material.TRAPPED_CHEST, world));
 				valid = valid && p.is(Material.IRON_BLOCK, world);
 				valid = valid && r.is(Material.DIAMOND_BLOCK, world);
 				valid = valid && f.is(Material.IRON_BLOCK, world);
@@ -180,7 +180,7 @@ public class GeneratorRunePattern {
 		valid = valid && model.signPoint.is(Material.WALL_SIGN, model.world);
 		valid = valid && model.anchorPoint.is(Material.DIAMOND_BLOCK, model.world);
 		valid = valid && model.wirePoint.is(Material.REDSTONE_WIRE, model.world);
-		valid = valid && model.chestPoint.is(Material.CHEST, model.world);
+		valid = valid && (model.chestPoint.is(Material.CHEST, model.world) || model.chestPoint.is(Material.TRAPPED_CHEST, model.world));
 
 		int goldCount = 0;
 		int ironCount = 0;
